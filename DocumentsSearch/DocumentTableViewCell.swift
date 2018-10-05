6667dc03de09ca1551b01816132ac34b9bc174f6
NewAndycul96 <andycul96@yahoo.com>
2018-10-05 16:23:31 -0500

    First Commit

---
+++ DocumentsSearch/DocumentTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DocumentTableViewCell.swift
+//  DocumentsSearch
+//
+//  Created by Anand Kulkarni on 10/5/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class DocumentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var modifiedLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DocumentsSearch/DocumentsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DocumentsViewController.swift
+//  DocumentsSearch
+//
+//  Created by Anand Kulkarni on 10/5/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class DocumentsViewController: UIViewController {
+
+    @IBOutlet weak var documentsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
